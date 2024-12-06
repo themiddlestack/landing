68d3d97a4b43d527a7835c6012fed8dff2aede91
ILYA <ya@MacBook-Air-I.local>
2024-12-06 09:13:29 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green name">MIDDLE STACK</h1>
+    <h3 class=title-text>
+      <span>Разработка SPA.</span> <br />
+      <span class="first-aria">Backend серверов.</span> <br />
+      <span class="second-aria">Телеграм ботов.</span>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 1024px) {
+    .name {
+      transform: translateX(30px);
+    }
+}
+.name {
+  font-famaly: "Ubuntu";
+  font-weight: 900;
+      color: rgb(0, 255, 200);
+}
+h1 {
+  transform: translateX(-100px);
+  font-weight: 500;
+  font-size: 2.6rem;
+  text-wrap: nowrap;
+  position: relative;
+  top: -10px;
+}
+h3 {
+  font-size: 1.2rem;
+}
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+.first-aria {
+  display: inline-block;
+  transform: translateX(30px);
+}
+.second-aria {
+  display: inline-block;
+  transform: translateX(60px);
+}
+.title-text {
+  transform: translateX(-15px)
+}
+.first-text {
+  color: rgba(0, 255, 195, .5);
+}
+.second-text {
+  color: rgba(255, 165, 0, .5);
+}
+.three-text {
+  color: rgba(255, 0, 0, .5);
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,73 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import CommunicationIcon from './icons/IconCommunication.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <CommunicationIcon />
+    </template>
+    <template #heading>
+      <span class="title">Коммуникация</span>
+    </template>
+    Общение в мессаджерах и на форуме. 
+    <a href="https://xss.is/escrow/" target="_blank" rel="noopener">Сделки - гарант.</a><br />
+    Согласование технического задания, сроков реализации планов клиента. <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>
+      <span class="title">Процесс</span>
+    </template>
+    Создаем структуру пректа. <br /> 
+    Расписываем поэтапно методы реализации идей. <br />
+    Настраиваем конфиги => Подбираем библиотеки => Пишем скрипты. 
+    <a href=https://www.w3schools.com target="_blank" rel="noopener">www.w3schools</a>
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>
+      <span class="title">Экосистема</span>
+    </template>
+    Поддердиваемые ЯП -- <a href=#>JavaScript - TypeScript - Golang</a><br />
+
+    Разработка ведется стого по специальной методологии. <br />
+
+    Скрипты пишутся на проф уровне. <br /> На выходе получаете продукт, который можно масштабировать.<br />
+
+    Поддерживаемые фреймворки 
+    <a href="https://vuejs.org" target="_blank" rel="noopener">Vue</a>,
+    <a href="https://react.dev" target="_blank" rel="noopener">React</a>,
+    <a href="https://expressjs.com" target="_blank" rel="noopener">Express</a>,
+    <a href="https://nestjs.com" target="_blank" rel="noopener">Nest</a>.
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>
+      <span class="title">Техподдержка</span>
+    </template>
+    Работаем в штатном режиме. 
+  </WelcomeItem>
+</template>
+
+<style scoped>
+  .title {
+    color: rgb(0, 255, 195);
+  }
+</style>
